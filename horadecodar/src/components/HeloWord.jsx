b26431424b8623e8319bfd12e81add7ee1edd015
Luís Silvério Calei Gabriel <132252803+luiscaleigabriel@users.noreply.github.com>
2024-06-17 19:19:09 +0100

    yha

---
+++ horadecodar/src/components/HeloWord.jsx
@@ -0,0 +1,12 @@
+import Frase from "./Frase"
+
+function HeloWord() {
+  return (
+    <div>    
+      <h1 className="gira">Helo world I'm here now</h1>
+      <Frase />
+    </div>
+  )
+}
+
+export default HeloWord

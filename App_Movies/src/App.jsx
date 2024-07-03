6bdc12a204a0251e70faa7f2f8c3bea45d4f2b13
Luís Silvério Calei Gabriel <132252803+luiscaleigabriel@users.noreply.github.com>
2024-07-03 17:44:47 +0100

    comitei

--- App_Movies/src/App.jsx
+++ App_Movies/src/App.jsx
@@ -3,6 +3,7 @@ import Footer from "./components/Footer"
 import Banner from "./components/Banner"
 import Containner from "./components/Containner"
 import Card from "./components/Card"
+import videos from './json/db.json'
 
 function App() {
 
@@ -13,30 +14,12 @@ function App() {
       <Containner >
         <h2>Geografia</h2>
         <section className="cards">
-          <Card 
-            to="https://www.youtube.com/watch?v=B_nq7VTJZds&list=PLnex8IkmReXwhwBi9av_8TrovNOivA6nu&index=1&pp=iAQB"
-            image="https://i.ytimg.com/vi/B_nq7VTJZds/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCtn3djCyVXzARItZEBkhXHOjclXQ"
-          />
-          <Card 
-            to="https://www.youtube.com/watch?v=8PjBddZ2cdo&list=PLnex8IkmReXwhwBi9av_8TrovNOivA6nu&index=2&pp=iAQB"
-            image="https://i.ytimg.com/vi/8PjBddZ2cdo/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLA2Xo31GCmVUXsYqRwTZQ5jddAAFA"
-          />
-          <Card 
-            to="https://www.youtube.com/watch?v=yqZsBn5Z1xc&list=PLnex8IkmReXwhwBi9av_8TrovNOivA6nu&index=3&pp=iAQB"
-            image="https://i.ytimg.com/vi/yqZsBn5Z1xc/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLAPsj8e-s8dseiCCo2n3tSZh1bqRg"
-          />
-          <Card 
-            to="https://www.youtube.com/watch?v=ee15tCw1kYo&list=PLnex8IkmReXwhwBi9av_8TrovNOivA6nu&index=4&pp=iAQB"
-            image="https://i.ytimg.com/vi/ee15tCw1kYo/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLD4WctOiQYSHWZDr6xZPDZGAmLGyA"
-          />
-          <Card 
-            to="https://i.ytimg.com/vi/QUc_M1b1uNE/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCB8d0xJTYarreewlibcJenrk5YIw"
-            image="https://i.ytimg.com/vi/QUc_M1b1uNE/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCB8d0xJTYarreewlibcJenrk5YIw"
-          />
-          <Card 
-            to="https://www.youtube.com/watch?v=YzBkkC2SXxM&list=PLnex8IkmReXwhwBi9av_8TrovNOivA6nu&index=6&pp=iAQB"
-            image="https://i.ytimg.com/vi/YzBkkC2SXxM/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLAeIqQSNjDmzfZsLHNVzmHGa4yhtQ"
-          />
+
+          {
+            videos.map(video => <Card url={video.url} image={video.cover} key={video.id} />
+            )
+          }
+          
         </section>
       </Containner>
       <Footer />
--- App_Movies/src/components/Card/index.jsx
+++ App_Movies/src/components/Card/index.jsx
@@ -1,9 +1,9 @@
 import style from './Card.module.css'
 
-function Card({ to, image }) {
+function Card({ image, title, url }) {
   return (
     <section className={style.card}>
-      <a href={to}
+      <a href={url}
       rel='noreferrer nooper'
       target='_blank'>
         <img src={image} alt="Capa" />

d2e268f06e7746ef72fa53b481f3134b9fd3341a
Luís Silvério Calei Gabriel <132252803+luiscaleigabriel@users.noreply.github.com>
2024-07-12 09:49:31 +0100

    Finalizei um novo proecto

--- Ecomerce_product_listing/src/components/ContentMain/index.jsx
+++ Ecomerce_product_listing/src/components/ContentMain/index.jsx
@@ -4,19 +4,21 @@ function ContentMain() {
   return (
     <section className="main_content">
       <h3>Produtos de Qualidade</h3>
-      { Cart.map((item) => (
-        <div className="card" key={item.id}>
-          <div className="card_img">
-            <img src={item.thumb} alt="product" />
+      <div className="cards">
+        { Cart.map((item) => (
+          <div className="card" key={item.id}>
+            <div className="card_img">
+              <img src={item.thumb} alt="product" />
+            </div>
+            <div className="card_header">
+              <h2>{item.name}</h2>
+              <p>{item.description}</p>
+              <p className="price">{item.price} <span>{item.currency}</span></p>
+              <div className="btn-add">Add no Carrinho</div>
+            </div>
           </div>
-          <div className="card_header">
-            <h2>{item.name}</h2>
-            <p>{item.description}</p>
-            <p className="price">{item.price} <span>{item.currency}</span></p>
-            <div className="btn-add">Add no Carrinho</div>
-          </div>
-        </div>
-      )) }
+        )) }
+      </div>
     </section>
   )
 }

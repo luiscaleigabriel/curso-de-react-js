56c23a239af446b2bc5c6e4ee51bf45c1aa9343e
Luís Silvério Calei Gabriel <132252803+luiscaleigabriel@users.noreply.github.com>
2024-08-30 11:23:36 +0100

    studing

--- App_Movies/src/App.jsx
+++ App_Movies/src/App.jsx
@@ -1,23 +1,23 @@
 import Banner from "./components/Banner"
 import Card from "./components/Card"
-import Category from "./components/Category"
+import Category, { categories, filterCategory } from "./components/Category"
 import Container from "./components/Container"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
-import videos from './json/db.json'
 
 function App() {
-  console.log(videos)
   return (
     <>  
       <Header />
       <Banner image='/images/banner-home.png' />
       <Container>
-        <Category category='Geografia'>
-          {videos.map(video => 
+        {categories.map((category, index) => {
+          return <Category category={category}>
+          {filterCategory(index).map(video => 
             <Card key={video.id} link={video.url} image={video.cover} />
           )}
         </Category>
+        })}
       </Container>
       <Footer />
     </>
--- App_Movies/src/components/Category/index.jsx
+++ App_Movies/src/components/Category/index.jsx
@@ -1,4 +1,15 @@
 import styles from './Category.module.css'
+import videos from '../../json/db.json'
+
+export const categories = [
+  'Geografia',
+  'História',
+  'Localização'
+]
+
+export function filterCategory(id) {
+  return videos.filter( video => video.category === categories[id] )
+}
 
 function Category({ category, children }) {
   return (
